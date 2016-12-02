5209816a6fbe0d32c70ef0a3c01b8b90b1b934ca
Matt Ellis <mellis@ncees.org>
2016-12-02 10:56:16 -0500

    #1 Landing page

--- src/CFA/LandingBundle/Controller/DefaultController.php
+++ src/CFA/LandingBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace CFA\LandingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/")
+ */
 class DefaultController extends Controller
 {
     /**
